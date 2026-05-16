c1840a25483e898091a3ed0766ec114ed2cb7776
aamanilcomps-ops <aam.anilcomps@gmail.com>
2026-05-17 01:15:32 +0530

    Add utility functions for dataset loading and preprocessing

---
+++ data/utils.py
@@ -0,0 +1,8 @@
+python
+import pandas as pd
+import numpy as np
+def load_dataset(path):
+    return pd.read_csv(path)
+def preprocess_data(data):
+    # add preprocessing steps here
+    return data
